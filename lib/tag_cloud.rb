b25d35608248f889999a8f2e58656f0baa42e628
Peter Hellberg <peter@c7.se>
2010-05-07 17:27:46 +0200

    Added the modified tag cloud class

---
+++ lib/tag_cloud.rb
@@ -0,0 +1,71 @@
+# Initial version by Masaki Yatsu <yatsu@yatsu.info>
+# Modified by Peter Hellberg <peter@c7.se>
+
+class TagCloud
+  def initialize
+    @counts = Hash.new
+    @urls = Hash.new
+    tags = []
+  end
+
+  def add(tag, url, count)
+    @counts[tag] = count
+    @urls[tag] = url
+  end
+
+  def css
+    text = "" 
+    for level in 0..18
+      font = 12 + level
+      text << "span.tagcloud#{level} {font-size: #{font}px;}\n" 
+      text << "span.tagcloud#{level} a {text-decoration: none;}\n" 
+    end
+    text
+  end
+
+  def html(limit = nil)
+    tags = @counts.sort_by {|a, b| b }.reverse.map {|a, b| a }
+    tags = tags[0..limit-1] if limit
+    if tags.empty?
+      return "" 
+    elsif tags.size == 1
+      tag = tags[0]
+      url = @urls[tag]
+      return %{<span class="tagcloud12"><a href='#{url}'>#{tag}</a></span>\n}
+    end
+
+    min = Math.sqrt(@counts[tags.last])
+    max = Math.sqrt(@counts[tags.first])
+    factor = 0
+
+    # special case all tags having the same count
+    if max - min == 0
+      min = min - 4
+      factor = 1
+    else
+      factor = 12 / (max - min)
+    end
+
+    html = ""
+    tag_count = 0
+    
+    tags.sort.each do |tag|
+      tag_count += 1
+      count = @counts[tag]
+      url   = @urls[tag]
+      level = ((Math.sqrt(count) - min) * factor).to_i
+      html << %{<span class="tagcloud#{level}"><a href='#{URI.escape(url)}'>#{tag}</a>}
+      html << ',' if tag_count < tags.size  
+      html << "</span>\n" 
+    end
+    html
+  end
+
+  def html_and_css(limit = nil)
+    "<div class=\"tagcloud\">\n<style>\n#{self.css}</style>\n#{self.html(limit)}\n</div>"
+  end
+  
+  def empty?
+    @counts.empty?
+  end
+end
